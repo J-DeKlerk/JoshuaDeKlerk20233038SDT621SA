343c49afe141186b7816e0e39dead4934e166059
J-DeKlerk <jdklerk321@gmail.com>
2026-05-27 11:58:21 +0200

    question 1.2 of the sdt621FA1

---
+++ SectionBQuestion1.2/SectionBQuestion1.2/Form1.cs
@@ -0,0 +1,28 @@
+namespace SectionBQuestion1._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+          Name = textBox1.Text;
+
+            MessageBox.Show("Hi there Mr/Mrs " + Name);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
